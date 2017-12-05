491d8ef018062278800763753905bef50c5a048a
NguyenSon <asanfc9x@gmail.com>
2017-12-05 17:56:17 +0700

    test1

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,7 @@
 class Home extends Public_Controller
 {
     function index() {
-        $this->data['name'] = 'This is data from Home controller';
+        $this->data['name'] = 'This is data from Home controller aaaa';
         $this->view('web/home');
     }
 }
