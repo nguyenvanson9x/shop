f4ba9fc1d8fcd3e13eb1862fe717f80053e3d14d
NguyenSon <asanfc9x@gmail.com>
2017-12-25 09:44:57 +0700

    change title

--- application/views/layout_web/include/head.php
+++ application/views/layout_web/include/head.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Document</title>
+<title>MobileShop</title>
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css' />
 <!-- Plugin css -->
 <link rel="stylesheet" href="<?= public_url()?>vendors/bootstrap.min.css">
