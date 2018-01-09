088e61bc47d9caf318d1f92b44b3a38b1726c328
NguyenSon <asanfc9x@gmail.com>
2018-01-09 07:16:25 +0700

    News controller

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -1,10 +1,32 @@
 <?php 
 class News extends Public_Controller
 {
-    
+    function __construct() {
+        parent::__construct();
+
+        // load model
+        $this->load->model('news_model');
+    }
     function index() {
-        $this->data['name'] = 'This is data from News controller';
+        // get data
+        $listNews = $this->news_model->get_list();
+        $this->data['listNews'] = $listNews;
+
+        // load view
         $this->view('web/news');
+    }
+
+    function detail($id = "") {
+        // check id
+        if ($id == "") {
+            // do something
+            // header('location: https://google.com');
+        }
+        else {
+            $news = $this->news_model->get_info($id);
+            $this->data['news'] = $news;
+            $this->view('web/news_detail');
+        }
     }
 }
  ?>
