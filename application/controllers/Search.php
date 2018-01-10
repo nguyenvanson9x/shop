8734771ec187d394c7ce5cf34af44ea5332e28b2
NguyenSon <asanfc9x@gmail.com>
2018-01-10 09:37:34 +0700

    Search modul

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -3,6 +3,19 @@
 class Search extends Public_Controller
 {
     function index() {
+        // load model
+        $this->load->model('product_model');
+
+        // get query
+        $query = $this->input->get('query');
+        $this->data['query'] = $query;
+        
+        // get data
+        $input['like'] = array('name', $query);
+        $products = $this->product_model->get_list($input);
+        $this->data['products'] = $products;
+
+        // load view
         $this->view('web/search');
     }
 }
--- application/views/web/search.php
+++ application/views/web/search.php
@@ -3,36 +3,78 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				{% if search.performed %}
-					{% if search.terms != blank %}
-						{% if search.results.size == 0 %}
+					<?php if ($query != ""):?>
+						<?php if (count($products) == 0):?>
 							<div class="bg-danger padding-15 margin-bottom-15">
-								<p class="margin-0">Không tìm thấy bất kỳ kết quả với từ khóa: <b class="keysearch">"{{ search.terms | escape }}"</b></p>	
+								<p class="margin-0">Không tìm thấy bất kỳ kết quả với từ khóa: <b class="keysearch">"<?php echo $query; ?>"</b></p>	
 							</div>
-						{% else %}
+						<?php else:?>
 							<div class="bg-success padding-15 margin-bottom-15">
-								<p class="margin-0">Có <b>{{ search.results.size }}</b> kết quả tìm kiếm với từ khoá <b>"{{ search.terms }}"</b></p>
+								<p class="margin-0">Có <b><?php echo count($products); ?></b> kết quả tìm kiếm với từ khoá <b>"<?php echo $query; ?>"</b></p>
 							</div>
 
 							<section class="section-products products-view products-view-grid lg4">
 								<div class="product-grid-show products">
 									<div class="row">
-										{% for product in search.results %}
+										<?php foreach ($products as $row):?>
 										<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 item" >
-											{% include 'product-grid-item' %}
+											<div class="product-box product-grid-item">
+												<div class="product-thumbnail">
+													<a href="<?php echo site_url('product/detail/'.$row->id)?>" title="<?php echo $row->name; ?>">
+														<picture class="dp-flex">
+															<img class="img-responsive" src="
+																	
+																	<?php
+																		if(file_exists('upload/product/'.$row->image))
+																		{
+																			echo base_url('upload/product/'.$row->image);
+																		}
+																		else
+																			{
+																			echo base_url('upload/unknown.png');
+																		}
+																	?>
+																	" alt="<?php echo $row->name; ?>" />
+														</picture>
+													</a>
+												</div>
+												<div class="product-info">
+													<h3 class="product-name text1line">
+														<a href="<?php echo site_url('product/detail/'.$row->id)?>" title="<?php echo $row->name; ?>">
+															<?php echo $row->name; ?>
+														</a>
+													</h3>
+													<div class="price-box price-loop-style">
+														<?php if ($row->discount != 0 && $row->expire_discount > now()) {?>
+														<span class="special-price">
+															<span class="price">
+																<?php echo number_format(($row->price-($row->price*$row->discount/100)),0,',','.'); ?>
+															</span>
+														</span>
+														<span class="old-price">
+															<span class="price">
+																<?php echo number_format($row->price,0,',','.'); ?>
+															</span>
+														</span>
+														<?php } else { ?>
+														<span class="special-price">
+															<span class="price">
+																<?php echo number_format($row->price,0,',','.'); ?>
+															</span>
+														</span>
+														<?php } ?>
+													</div>
+												</div>
+											</div>
 										</div>
-										{% endfor %}
+										<?php endforeach;?>
 									</div>
 								</div>
 							</section>
-
-
-						{% endif %}
-					{% else %}
+						<?php endif;?>
+						<?php else:?>
 						<div class="bg-danger padding-15 margin-bottom-15">Vui lòng nhập từ khóa tìm kiếm</div>
-						{% include 'search-module' %}
-					{% endif %}
-				{% endif %}
+						<?php endif;?>
 			</div>
 		</div>
 	</div>
