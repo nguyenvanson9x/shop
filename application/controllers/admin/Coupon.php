4cc16295dab83e1c1395d7572dbfae84fa25bad9
NguyenSon <asanfc9x@gmail.com>
2018-01-10 20:19:38 +0700

    update coupon modul

--- application/controllers/admin/Coupon.php
+++ application/controllers/admin/Coupon.php
@@ -15,7 +15,46 @@ Class Coupon extends MY_Controller
     }
 
     function add() {
-      $this->render('admin/coupon/add');
+        //load thư viện validate dữ liệu
+        $this->load->library('form_validation');
+        $this->load->helper('form');
+        
+        //neu ma co du lieu post len thi kiem tra
+        if($this->input->post())
+        {
+            $this->form_validation->set_rules('code', 'Mã khuyến mãi', 'required');
+            $this->form_validation->set_rules('discount', 'Giá trị khuyến mãi', 'required');
+            $this->form_validation->set_rules('begin_at', 'Ngày bắt đầu', 'required');
+            $this->form_validation->set_rules('expire_at', 'Ngày kết thúc', 'required');
+            //nhập liệu chính xác
+            if($this->form_validation->run())
+            {
+                //them vao csdl
+                $code = $this->input->post('code');
+                $discount = $this->input->post('discount');
+                $begin_at = $this->input->post('begin_at');
+                $expire_at = $this->input->post('expire_at');
+    
+                //luu du lieu can them
+                $data = array(
+                    'code'      => $code,
+                    'discount' => $discount,
+                    'begin_at' => get_time_from_date($begin_at),
+                    'expire_at' => get_time_from_date($expire_at)
+                );
+                //them moi vao csdl
+                if($this->coupon_model->create($data))
+                {
+                    //tạo ra nội dung thông báo
+                    $this->session->set_flashdata('message', 'Cập nhật dữ liệu thành công');
+                }else{
+                    $this->session->set_flashdata('message', 'Không cập nhật được');
+                }
+                //chuyen tới trang danh sách
+                redirect(admin_url('coupon'));
+            }
+        }    
+        $this->render('admin/coupon/add');
     }
 
     function edit() {
--- application/views/admin/coupon/add.php
+++ application/views/admin/coupon/add.php
@@ -4,29 +4,75 @@
 <div class="line"></div>
 
 <div class="wrapper">
-      <div class="widget">
-           <div class="title">
+	<div class="widget">
+		<div class="title">
 			<h6>Thêm mới loại sản phẩm</h6>
 		</div>
 
-      <form id="form" class="form" enctype="multipart/form-data" method="post" action="">
-          <fieldset>
-                
-                <div class="formRow">
-                	<label for="param_code" class="formLeft">Mã khuyến mãi:</label>
-                	<div class="formRight">
-                		<span class="oneTwo"><input type="text" _autocheck="true" id="param_code" value="<?php echo set_value('code')?>" name="code"></span>
-                		<span class="autocheck" name="name_autocheck"></span>
-                		<div class="clear error" name="name_error"><?php echo form_error('code')?></div>
-                	</div>
-                	<div class="clear"></div>
-                </div>
-                            
-                <div class="formSubmit">
-	           			<input type="submit" class="redB" value="Thêm mới">
-	           	</div>
-          </fieldset>
-      </form>
-      
-      </div>
+		<form id="form" class="form" enctype="multipart/form-data" method="post" action="">
+			<fieldset>
+
+				<div class="formRow">
+					<label for="param_code" class="formLeft">Mã khuyến mãi:</label>
+					<div class="formRight">
+						<span class="oneTwo">
+							<input type="text" _autocheck="true" id="param_code" value="" name="code">
+						</span>
+						<span class="autocheck" name="code_autocheck"></span>
+						<div class="clear error" name="code_error">
+							<?php echo form_error('code')?>
+						</div>
+					</div>
+					<div class="clear"></div>
+				</div>
+
+				<div class="formRow">
+					<label for="param_discount" class="formLeft">Giá trị khuyến mãi (%):</label>
+					<div class="formRight">
+						<span class="oneTwo">
+							<input type="text" _autocheck="true" id="param_discount" value="" name="discount">
+						</span>
+						<span class="autocheck" name="discount_autocheck"></span>
+						<div class="clear error" name="discount_error">
+							<?php echo form_error('discount')?>
+						</div>
+					</div>
+					<div class="clear"></div>
+				</div>
+
+				<div class="formRow">
+					<label for="param_begin_at" class="formLeft">Ngày bắt đầu:</label>
+					<div class="formRight">
+						<span class="oneTwo">
+							<input type="text" _autocheck="true" id="param_begin_at" value="" name="begin_at" class="datepicker">
+						</span>
+						<span class="autocheck" name="begin_at_autocheck"></span>
+						<div class="clear error" name="begin_at_error">
+							<?php echo form_error('begin_at')?>
+						</div>
+					</div>
+					<div class="clear"></div>
+				</div>
+
+				<div class="formRow">
+					<label for="param_expire_at" class="formLeft">Ngày kết thúc:</label>
+					<div class="formRight">
+						<span class="oneTwo">
+							<input type="text" _autocheck="true" id="param_expire_at" value="" name="expire_at" class="datepicker">
+						</span>
+						<span class="autocheck" name="expire_at_autocheck"></span>
+						<div class="clear error" name="expire_at_error">
+							<?php echo form_error('expire_at')?>
+						</div>
+					</div>
+					<div class="clear"></div>
+				</div>
+
+				<div class="formSubmit">
+					<input type="submit" class="redB" value="Thêm mới">
+				</div>
+			</fieldset>
+		</form>
+
+	</div>
 </div>
